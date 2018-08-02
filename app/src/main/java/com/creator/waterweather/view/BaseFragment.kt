ccad050afe9fb682e67f2e0a06fe335e33c4b342
Felix.Liang <lys74123@126.com>
2018-08-02 15:56:36 +0800

    Update base activity and fragment

--- app/src/main/java/com/creator/waterweather/view/BaseFragment.kt
+++ app/src/main/java/com/creator/waterweather/view/BaseFragment.kt
@@ -1,6 +1,16 @@
 package com.creator.waterweather.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.trello.rxlifecycle2.components.support.RxFragment
 
 abstract class BaseFragment : RxFragment() {
+
+    abstract val layoutResID: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layoutResID, container, false)
+    }
 }
