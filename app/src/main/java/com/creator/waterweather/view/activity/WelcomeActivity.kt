8969ed3f68fea5faee7a726ac6ae6c5e3550e247
Felix.Liang <lys74123@126.com>
2018-08-02 16:11:33 +0800

    Add welcome page

---
+++ app/src/main/java/com/creator/waterweather/view/activity/WelcomeActivity.kt
@@ -0,0 +1,9 @@
+package com.creator.waterweather.view.activity
+
+import com.creator.waterweather.R
+import com.creator.waterweather.view.BaseActivity
+
+class WelcomeActivity : BaseActivity() {
+
+    override val layoutResID: Int = R.layout.activity_welcome
+}
