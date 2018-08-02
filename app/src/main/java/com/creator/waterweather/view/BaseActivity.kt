5a08e6ff6091a306fc3de8df2ed86f4fb6530bbb
Felix.Liang <lys74123@126.com>
2018-08-02 15:34:22 +0800

    Define the base activity class and fragment class

---
+++ app/src/main/java/com/creator/waterweather/view/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.creator.waterweather.view
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+
+abstract class BaseActivity : RxAppCompatActivity()
---
+++ app/src/main/java/com/creator/waterweather/view/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.creator.waterweather.view
+
+import com.trello.rxlifecycle2.components.support.RxFragment
+
+abstract class BaseFragment : RxFragment() {
+}
