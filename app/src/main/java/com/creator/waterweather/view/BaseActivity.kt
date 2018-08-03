5fe8c8fc8d8b220c3c18c4d3fb8fc1ca18d43e9d
Felix.Liang <lys74123@126.com>
2018-08-03 14:03:21 +0800

    Update base activity

--- app/src/main/java/com/creator/waterweather/view/BaseActivity.kt
+++ app/src/main/java/com/creator/waterweather/view/BaseActivity.kt
@@ -1,14 +1,5 @@
 package com.creator.waterweather.view
 
-import android.os.Bundle
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
-abstract class BaseActivity : RxAppCompatActivity() {
-
-    abstract val layoutResID: Int
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(layoutResID)
-    }
-}
+abstract class BaseActivity : RxAppCompatActivity()
