c106287ff8ec87ee2e4d807ca4237f64d8c463a1
Felix.Liang <lys74123@126.com>
2018-08-06 14:21:35 +0800

    Define some useful constants

---
+++ app/src/main/java/com/creator/waterweather/extension/Constants.kt
@@ -0,0 +1,7 @@
+package com.creator.waterweather.extension
+
+const val KEY_LAUNCH = "launchState"
+
+const val APP_DATABASE_NAME = "water_weather.db"
+
+const val MAX_HTTP_CACHE_SIZE = 10L * 1024 * 1024
