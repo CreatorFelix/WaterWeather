cc4e8ed8e02411464cc322c2f1acc21da5feaf1b
Felix.Liang <lys74123@126.com>
2018-08-09 16:53:12 +0800

    Define scheduler provider

---
+++ app/src/main/java/com/creator/waterweather/util/BaseSchedulerProvider.kt
@@ -0,0 +1,12 @@
+package com.creator.waterweather.util
+
+import io.reactivex.Scheduler
+
+interface BaseSchedulerProvider {
+
+    fun computation(): Scheduler
+
+    fun io(): Scheduler
+
+    fun ui(): Scheduler
+}
